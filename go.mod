65455e41ee38d3f48a9429bb08b7421991be244b
Spencer Williams <s@spencerenglish.com>
2021-07-13 13:23:26 -0700

    wip

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/spilliams/tunnelvision
 go 1.15
 
 require github.com/spf13/cobra v1.1.3
+
+require github.com/hashicorp/hcl/v2 v2.8.2
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -1,9 +1,11 @@
 package cli
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/spf13/cobra"
 )
 
@@ -11,13 +13,13 @@ func newRootCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "tunnelvision",
 		Short: "A way to visualize your terraform plans",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			log.Println("hello, world")
-			return nil
-		},
+		// RunE: func(cmd *cobra.Command, args []string) error {
+		// 	log.Println("hello, world")
+		// 	return nil
+		// },
 	}
 
-	// cmd.AddCommand(cmds ...*cobra.Command)
+	cmd.AddCommand(newSimpleParseCommand())
 
 	return cmd
 }
@@ -26,5 +28,20 @@ func Execute() {
 	if err := newRootCmd().Execute(); err != nil {
 		log.Println(err)
 		os.Exit(1)
+	}
+}
+
+func newSimpleParseCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "parse FILE",
+		Short: "Parse a single hcl file",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			p := hclparse.NewParser()
+			file, diagnostics := p.ParseHCLFile(args[0])
+			fmt.Println(file)
+			fmt.Println(diagnostics)
+			return nil
+		},
 	}
 }
